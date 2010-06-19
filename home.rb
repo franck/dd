fb24df10f5ddd879a241833f80907a1112f7c839
Franck <franck@franck.(none)>
2010-06-19 09:42:40 +0200

    first commit

---
+++ home.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+
+before do
+  headers "Content-Type" => "text/html; charset=utf-8"
+end
+
+get '/' do
+  haml :home_index
+end
+
+get '/styles.css' do
+  response['Content-Type'] = "text/css; charset=utf-8"
+  sass :styles
+end
